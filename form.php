2b31953b2e67ac6399729c1edebc3e79c6fbd140
Braydenjlee <Braydenlee@college.harvard.edu>
2024-12-08 14:05:24 -0500

    Update form.php

--- form.php
+++ form.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 /*create these variables to send to the email*/
-$email_from = 'braydenlee@college.harvard.edu';
+$email_from = 'info@braydenjlee.github.io';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "user Email: $email.\n".
